490badcbe88a37244af01fee593f465cc9b3c7fb
chitralimbu <limbu_nikkil@hotmail.co.uk>
2021-01-14 23:11:47 +0000

    initial commit

---
+++ src/main/java/com/tutorial/session/SpringSessionTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/session/SpringSessionTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
