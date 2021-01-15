fbb2b379cdbff5542d077f1ef96331755164f752
chitralimbu <limbu_nikkil@hotmail.co.uk>
2021-01-15 00:21:35 +0000

    MongoDb Session completed

---
+++ src/main/java/com/tutorial/session/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.tutorial.session.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String getHome(){
+        return "home";
+    }
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "test";
+    }
+}
